2e7dfae0eb093240fc95dc377dba502464a5587b
Daniel Huchani <161368230+HuchaniDev@users.noreply.github.com>
2025-02-20 19:14:36 -0400

    feat: add wiews

--- src/app/Schedule/services/schedule.service.ts
+++ src/app/Schedule/services/schedule.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Schedule } from "../models/schedule.interface";
 import { ResponseInterface } from "../models/Response.Interface";
+import { catchError, throwError } from "rxjs";
 
 @Injectable({
   providedIn : 'root'
@@ -12,6 +13,20 @@ export  class ScheduleService {
   #endPoint="http://localhost:5050/schedules";
 
   getSchedule$(userId:number) {
-    return this.#http.get<ResponseInterface<Schedule[]>>(`${this.#endPoint}/by-user/${userId}`);
+    return this.#http.get<ResponseInterface<Schedule[]>>(`${this.#endPoint}/by-user/${userId}`)
+    .pipe(
+      catchError(this.#handleError)
+    );
   }
+
+  #handleError(error: HttpErrorResponse) {
+		let errorMessage = '';
+		if (error.error instanceof ErrorEvent) {
+			errorMessage = `Error fontend: ${error.error.message}`;      
+		} else {
+			errorMessage = `Error backend Code: ${error.status}\nMessage: ${JSON.stringify(error.error)}`;
+    }
+    console.log(errorMessage);
+		return throwError(()=> error);
+	}
 }
--- src/app/Schedule/views/schedule/index.component.ts
+++ src/app/Schedule/views/schedule/index.component.ts
@@ -3,21 +3,51 @@ import { ScheduleService } from "../../services/schedule.service";
 import { ResponseInterface } from "../../models/Response.Interface";
 import { Schedule } from "../../models/schedule.interface";
 import { log } from "console";
+import { FormsModule } from "@angular/forms";
+import { User } from "../../models/User.interface";
 
 @Component({
   selector: "app-schedule",
   standalone: true,
   templateUrl: "./index.component.html",
+  imports: [
+    FormsModule
+  ]
 })
 export default class ScheduleComponent {
   #scheduleService = inject(ScheduleService);
   title = "Schedule";
 
+  users:User[]=[
+    {
+      id:0,
+      name:"daniel",
+      lastName:"huchani Huaranca"
+    },
+    {
+      id:1,
+      name:"daniel2",
+      lastName:"huchani Huaranca"
+    },
+    {
+      id:2,
+      name:"daniel3",
+      lastName:"huchani Huaranca"
+    }
+
+  ]
+
+  user:User = {
+    id:0,
+    name:"daniel",
+    lastName:"huchani Huaranca"
+  }
+
   userId = 1;
   schdules:Schedule[] = [];
 
   constructor() {
-    this.getSchedule();
+    //this.getSchedule();
   }
   
   getSchedule() {
@@ -27,10 +57,11 @@ export default class ScheduleComponent {
         if(response.isSuccess) {
           this.schdules = response.data;
         }
-        else {
-          console.log(response.message);
-        }
       },
+      error: (error) => {
+        console.log("error",error);
+        this.schdules = [];
+      }
     });
   }
 
